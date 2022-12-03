74d9ab763451a5bfc808418ef9e69ca97889672f
Wojciech Pająk <arachnomorph@gmail.com>
2022-12-03 21:28:08 +0100

    login form

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useForm } from 'react-hook-form';
 
 const LogIn = () => {
-    return <h1>LogIn</h1>;
-  };
-  
-  export default LogIn;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder='email' type='email' {...register("email", { required: true, minLength: 3, pattern: /[a-zA-z0-9]@[a-zA-z0-9]/g })} />
+      <input placeholder='password' type='password' {...register("password", { required: true, minLength: 3 })} />
+      <input type="submit" />
+    </form>
+  )
+};
+
+export default LogIn;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,7 +3,7 @@ import styles from '.././scss/styles.module.scss'
 
 const Main = () => {
     return (<div className={styles.mainContainer}>
-        <img src="https://picsum.photos/1000" />
+        <img src="https://picsum.photos/300" />
         <p>Integer efficitur condimentum odio, at consequat ante interdum ac. Nam ac felis ac enim eleifend semper sed eu quam. Nulla turpis enim, tempus ac diam maximus, fermentum condimentum arcu. Duis semper neque a rutrum accumsan. Nulla sed erat sed massa iaculis lacinia. Ut neque turpis, consectetur non consectetur quis, tempor non eros. Sed odio quam, varius vitae orci sed, auctor rutrum erat. Ut id laoreet augue. Phasellus ac vehicula dolor. Donec ac tortor a ipsum sollicitudin ultrices et eget nunc. Proin et magna congue, bibendum ante sit amet, condimentum justo.</p>
     </div>
     )
